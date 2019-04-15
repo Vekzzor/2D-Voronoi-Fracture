d92c32f9f6f83df51bc660085d9ef5401588cac3
Unknown <vikzzan@gmail.com>
2019-04-15 16:52:31 +0200

    Added rendering of Voronoi edges

--- Kandidatarbete/main.cpp
+++ Kandidatarbete/main.cpp
@@ -1,8 +1,41 @@
 
 #include <SFML/Graphics.hpp>
+#include "voronoi.h"
+
+Voronoi* vdg;
+vector<VoronoiPoint*> ver;
+vector<VEdge> edges;
 int main()
 {
+	//FORTUNES ALGORITHM
+	for (vector<VoronoiPoint*>::iterator i = ver.begin(); i != ver.end(); i++)
+		delete((*i));
+	ver.clear();
+	edges.clear();
+	for (int i = 0; i < 100; i++)
+	{
+		ver.push_back(new VoronoiPoint(rand() % 500, rand() % 500));
+	}
+
+
+
+	vdg = new Voronoi();
+	double minY = 0;
+	double maxY = 500;
+	edges = vdg->ComputeVoronoiGraph(ver, minY, maxY);
+	delete vdg;
+	std::vector<sf::Vertex*> lines;
+	for (int i = 0; i < edges.size(); i++)
+	{
+		sf::Vertex* test = new sf::Vertex[2];
+		test[0] = sf::Vector2f((edges[i].VertexA.x), (edges[i].VertexA.y));
+		test[1] = sf::Vector2f((edges[i].VertexB.x), (edges[i].VertexB.y));
+		lines.push_back(test);
+	}
+	
+
 	sf::RenderWindow window(sf::VideoMode(500, 500), "SFML works!");
+	
 	sf::CircleShape shape(100.f);
 	shape.setFillColor(sf::Color::Green);
 
@@ -25,7 +58,9 @@ int main()
 		}
 
 		window.clear();
-		window.draw(shape);
+		//window.draw(shape);
+		for(auto line : lines)
+			window.draw(line, 2, sf::Lines);
 		window.display();
 	}
 
